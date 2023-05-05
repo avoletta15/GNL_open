a0ccc6ce34e0e4794decb8756aa3c84e57e2032e
Maria Avoletta <mariaavoletta@Marias-MacBook-Air.local>
2023-05-05 12:00:41 +0100

    KO abort

--- get_next_line.c
+++ get_next_line.c
@@ -12,28 +12,6 @@
 
 #include "get_next_line.h"
 
-/* char	*looping(char *line)
-{
-	char	*backup;
-	size_t	i;
-
-	i = 0;
-	while (line[i] != '\0' && line[i] != '\n')
-		i++;
-	if (line[i] == '\0') // quando a linha em si ja nao tem nada
-		return (NULL);
-	backup = ft_substr(line, i + 1, ft_strlen(line) - i);
-	if (*backup == '\0') // para ter certeza que o byte apos a quebra de linha
-	// ou o suposto final da string eh mesmo o final
-	{
-		free(backup);
-		return (NULL);
-	}
-	line[i + 1] = '\0';
-	return (backup);
-} */
-
-
 char	*looping(char *line)
 {
 	char	*backup;
